d14eefa6db1665c11f2f338341d7061b514c6011
Ben Clifford <benc@hawaga.org.uk>
2025-09-28 10:30:05 +0000

    typo

--- tests/unit/handle_test.py
+++ tests/unit/handle_test.py
@@ -186,7 +186,7 @@ async def test_client_handle_ping_timeout(
 
 
 @pytest.mark.asyncio
-async def test_client_handle_shutdown_ignore_already_termiated_error() -> None:
+async def test_client_handle_shutdown_ignore_already_terminated_error() -> None:
     handle: Handle[EmptyAgent] = Handle(AgentId.new())
 
     request = Message.create(

db6223052a2f9e9006f848f0d06dfacd811cdfdf
AK2000 <10855694+AK2000@users.noreply.github.com>
2025-11-14 14:42:32 -0600

    Fixing race in handle (#280)
    * Fixing race in handle
    
    * Changing to check the status of future

--- academy/handle.py
+++ academy/handle.py
@@ -190,17 +190,17 @@ class Handle(Generic[AgentT]):
             return
 
         future = self._pending_response_futures.pop(response.tag)
-        assert not future.cancelled()
 
-        body = response.get_body()
-        if isinstance(body, ActionResponse):
-            future.set_result(body.get_result())
-        elif isinstance(body, ErrorResponse):
-            future.set_exception(body.get_exception())
-        elif isinstance(body, SuccessResponse):
-            future.set_result(None)
-        else:
-            raise AssertionError('Unreachable.')
+        if not future.cancelled():
+            body = response.get_body()
+            if isinstance(body, ActionResponse):
+                future.set_result(body.get_result())
+            elif isinstance(body, ErrorResponse):
+                future.set_exception(body.get_exception())
+            elif isinstance(body, SuccessResponse):
+                future.set_result(None)
+            else:
+                raise AssertionError('Unreachable.')
 
     def _register_with_exchange(self, exchange: ExchangeClient[Any]) -> None:
         """Register to receive messages from exchange.
@@ -255,7 +255,7 @@ class Handle(Generic[AgentT]):
                 'academy.action': action,
             },
         )
-        await asyncio.shield(future)
+        await future
         return future.result()
 
     async def ping(self, *, timeout: float | None = None) -> float:
@@ -286,7 +286,6 @@ class Handle(Generic[AgentT]):
         )
         loop = asyncio.get_running_loop()
         future: asyncio.Future[None] = loop.create_future()
-        future = asyncio.shield(future)
         self._pending_response_futures[request.tag] = future
         start = time.perf_counter()
         await self.exchange.send(request)

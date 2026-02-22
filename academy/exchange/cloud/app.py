0fd3101b49b1b17631e260c3fa33b5a517108725
Ben Clifford <benc@hawaga.org.uk>
2026-02-22 13:55:49 +0000

    Add missing f to log f-string

--- academy/exchange/cloud/app.py
+++ academy/exchange/cloud/app.py
@@ -456,7 +456,7 @@ async def _recv_message_route(request: Request) -> Response:  # noqa: PLR0911
             text='Incorrect permissions',
         )
     except TimeoutError:
-        logger.exception('Receive from mailbox {mailbox_id} timed-out.')
+        logger.exception(f'Receive from mailbox {mailbox_id} timed-out.')
         return Response(
             status=StatusCode.TIMEOUT.value,
             text='Request timeout',

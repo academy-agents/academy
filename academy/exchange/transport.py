e32f340c3035bb603f7ee9cbb9db76b3e16086ac
Jason Zhang <zwenz@uchicago.edu>
2026-03-26 23:58:34 -0500

    Added query by string for discover methods across exchange platforms - Issue #250
    Changes to meet PR comments

--- academy/exchange/transport.py
+++ academy/exchange/transport.py
@@ -104,9 +104,8 @@ class ExchangeTransport(Protocol[AgentRegistrationT_co]):
             of all agents registered to the exchange.
 
         Args:
-            agent: Agent type of interest or qualified class name string
-                (e.g.'mypackage.MyAgent')
-
+            agent: Agent type of interest qualified class name string (e.g.
+            'mypackage.MyAgent')
             allow_subclasses: Return agents implementing subclasses of the
                 agent.
 
